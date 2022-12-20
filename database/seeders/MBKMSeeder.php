f12da3d3aa8d8175a3d6a0d07b26e71480cbf5f6
Farhan Hanif <38966867+Chillrend@users.noreply.github.com>
2022-12-20 08:22:39 +0700

    Update MBKMSeeder.php
    Updated model MBKM according to Program Kemendikbud

--- database/seeders/MBKMSeeder.php
+++ database/seeders/MBKMSeeder.php
@@ -16,13 +16,13 @@ class MBKMSeeder extends Seeder
     public function run()
     {
         DB::table('modelmbkms')->insert([
-            ['id' => 1, 'model_mbkm' => 'Pertukaran Pelajar'],
-            ['id' => 2, 'model_mbkm' => 'Magang / Praktik Kerja'],
-            ['id' => 3, 'model_mbkm' => 'Asistensi Mengajar'],
+            ['id' => 1, 'model_mbkm' => 'Pertukaran Mahasiswa (PMM/IISMA/IISMAVO)'],
+            ['id' => 2, 'model_mbkm' => 'Magang / Praktik Kerja / Magang Bersertifikat (MSIB)'],
+            ['id' => 3, 'model_mbkm' => 'Asistensi Mengajar (Praktisi Mengajar / Kampus Mengajar)'],
             ['id' => 4, 'model_mbkm' => 'Riset / Penelitian'],
             ['id' => 5, 'model_mbkm' => 'Proyek Kemanusiaan'],
-            ['id' => 6, 'model_mbkm' => 'Kegiatan Wirausaha'],
-            ['id' => 7, 'model_mbkm' => 'Studi / Proyek Independen'],
+            ['id' => 6, 'model_mbkm' => 'Wirausaha Merdeka'],
+            ['id' => 7, 'model_mbkm' => 'Studi Independen Bersertifikasi (MSIB)'],
             ['id' => 8, 'model_mbkm' => 'KKN Tematik'],
         ]);
     }
